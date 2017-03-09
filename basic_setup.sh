2e17370cd25e55b3cba152aeb3efc2b9bbb36cff
Tim Witteveen <timw@pnnl.gov>
2017-03-08 23:09:24 -0800

    a couple files

---
+++ basic_setup.sh
@@ -0,0 +1,23 @@
+# TomS's stuff# ln -sf ~/.my-osx-setup/bashrc ~/.bashrc
+# TomS's stuff# ln -sf ~/.my-osx-setup/bash_aliases ~/.bash_aliases
+# TomS's stuff# ln -sf ~/.my-osx-setup/screen-keys.conf ~/.tmux.conf
+# TomS's stuff# ln -sf ~/.my-osx-setup/gitconfig ~/.gitconfig
+# TomS's stuff# ln -sf ~/.my-osx-setup/vimrc ~/.vimrc
+# TomS's stuff# touch ~/.localbashrc
+# TomS's stuff# touch ~/.localbash_aliases
+# TomS's stuff# touch ~/.localgitconfig
+# TomS's stuff# touch ~/.localvimrc
+# TomS's stuff#
+# TomS's stuff# git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+# TomS's stuff#
+# TomS's stuff# bash -c "base16_google-dark"
+# TomS's stuff#
+# TomS's stuff# git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# TomS's stuff# vim +PluginInstall +qall
+
+ln -sf ~/.my-osx-setup/calendar ~/.calendar
+ln -sf ~/.my-osx-setup/bin ~/bin
+ln -sf ~/.my-osx-setup/bash_profile ~/.bash_profile
+ln -sf ~/.my-osx-setup/bashrc ~/.bashrc
+ln -sf ~/.my-osx-setup/vimrc ~/.vimrc
+ln -sf ~/.my-osx-setup/x2goclient ~/.x2goclient
