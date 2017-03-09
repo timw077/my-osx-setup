070b4e5814cc8b6eb9836f4a4646214bf815cb51
Tim Witteveen <timw@pnnl.gov>
2017-03-09 13:27:09 -0800

    adding atom, and alphabetise things.

--- basic_setup.sh
+++ basic_setup.sh
@@ -15,9 +15,19 @@
 # TomS's stuff# git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # TomS's stuff# vim +PluginInstall +qall
 
-ln -sf ~/.my-osx-setup/calendar ~/.calendar
 ln -sf ~/.my-osx-setup/bin ~/bin
+
+# atom plugins - I want them to just work.
+mkdir ~/.atom
+ln -sf ~/.my-osx-setup/atom/packages/ ~/.atom/packages
+
 ln -sf ~/.my-osx-setup/bash_profile ~/.bash_profile
 ln -sf ~/.my-osx-setup/bashrc ~/.bashrc
+
+ln -sf ~/.my-osx-setup/calendar ~/.calendar
+
+ln -sf ~/.my-osx-setup/gitconfig ~/.gitconfig
+
 ln -sf ~/.my-osx-setup/vimrc ~/.vimrc
 ln -sf ~/.my-osx-setup/x2goclient ~/.x2goclient
+ln -sf ~/.my-osx-setup/Xmodmap ~/.Xmodmap
