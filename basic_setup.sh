d77bb56227054cd5f7a8c9ab2a664cad4a55d7a5
Tim Witteveen <timw@pnnl.gov>
2017-03-09 15:27:18 -0800

    Touching local resources.

--- basic_setup.sh
+++ basic_setup.sh
@@ -1,12 +1,5 @@
-# TomS's stuff# ln -sf ~/.my-osx-setup/bashrc ~/.bashrc
 # TomS's stuff# ln -sf ~/.my-osx-setup/bash_aliases ~/.bash_aliases
 # TomS's stuff# ln -sf ~/.my-osx-setup/screen-keys.conf ~/.tmux.conf
-# TomS's stuff# ln -sf ~/.my-osx-setup/gitconfig ~/.gitconfig
-# TomS's stuff# ln -sf ~/.my-osx-setup/vimrc ~/.vimrc
-# TomS's stuff# touch ~/.localbashrc
-# TomS's stuff# touch ~/.localbash_aliases
-# TomS's stuff# touch ~/.localgitconfig
-# TomS's stuff# touch ~/.localvimrc
 # TomS's stuff#
 # TomS's stuff# git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 # TomS's stuff#
@@ -14,6 +7,11 @@
 # TomS's stuff#
 # TomS's stuff# git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # TomS's stuff# vim +PluginInstall +qall
+
+touch ~/.localbashrc
+touch ~/.localbash_aliases
+touch ~/.localgitconfig
+touch ~/.localvimrc
 
 ln -sf ~/.my-osx-setup/bin ~/bin
 
